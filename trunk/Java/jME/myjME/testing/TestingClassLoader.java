0f92194e98b2c0cac6cb420a345f85c7aec1c96b
Robert.Gresdal@gmail.com <Robert.Gresdal@gmail.com@bb0b4684-9848-0410-a503-375951d3d4f2>
2008-04-18 22:04:19 +0000

    Moved loading of a dynamic class to a function for easies sake.

--- trunk/Java/jME/myjME/testing/TestingClassLoader.java
+++ trunk/Java/jME/myjME/testing/TestingClassLoader.java
@@ -8,21 +8,54 @@ public class TestingClassLoader {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		try{
-			//Class type = ClassLoader.getSystemClassLoader().loadClass("myjME.testing.DynamicClass1");
-			Class type = SecureClassLoader.getSystemClassLoader().loadClass("myjME.testing.DynamicClass1");
-			ICanAddNumbers foo = (ICanAddNumbers)type.newInstance();
-			System.out.println(foo.add(2, 3));
-			type = ClassLoader.getSystemClassLoader().loadClass("myjME.testing.DynamicClass2");
-			System.out.println(foo.add(2, 3));
-			foo = (ICanAddNumbers)type.newInstance();
-			System.out.println(foo.add(2, 3));
-		} catch(Exception badHandling){
-			System.out.println(badHandling);
-		} 
+		TestingClassLoader t = new TestingClassLoader();
+		t.start();
 	}
-
+	
+	public void start(){
+		try{
+			ICanAddNumbers foo = getAddClass(1);
+			System.out.println(foo.add(2, 3)); // expected output "5"
+			foo = getAddClass(2); 
+			System.out.println(foo.add(2, 3)); // expected output "32"
+			foo = getAddClass(3); // expected to catch exception here
+		} catch(PluginNotLoadedException e){
+			System.out.println("Unable to load plugin: "+e); 
+		}
+	}
+	
+	private ICanAddNumbers getAddClass(int i) throws PluginNotLoadedException {
+		Class<ICanAddNumbers> loaded;
+		ICanAddNumbers object;
+		try {
+			ClassLoader cl = SecureClassLoader.getSystemClassLoader();
+			if(i==1){
+				loaded = (Class<ICanAddNumbers>)(cl.loadClass("myjME.testing.DynamicClass1"));
+			} else if(i==2) {
+				loaded = (Class<ICanAddNumbers>)(cl.loadClass("myjME.testing.DynamicClass2"));
+			} else {
+				loaded = (Class<ICanAddNumbers>)(cl.loadClass("myjME.testing.DynamicClass3"));
+			}
+			object = (ICanAddNumbers)loaded.newInstance();
+		} catch(ClassNotFoundException e){
+			throw new PluginNotLoadedException("Plugin class not found: DynamicClass"+i);
+		} catch(IllegalAccessException e){
+			throw new PluginNotLoadedException("Caught IllegalAccessException when loading the plugin DynamicClass"+i);
+		} catch(InstantiationException e){
+			throw new PluginNotLoadedException("Caught InstantiationException when loading the plugin DynamicClass"+i);
+		}
+		
+		return object;
+	}
+	
+	class PluginNotLoadedException extends Exception {
+		public PluginNotLoadedException(String message) {
+            // Constructor.  Create a ParseError object containing
+            // the given message as its error message.
+			super(message);
+		}
+	}
+	
 	public interface ICanAddNumbers {
 		public int add(int a, int b);
 	}
