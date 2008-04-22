fd4eee0ed81e26989bde221879084ced61860801
Robert.Gresdal@gmail.com <Robert.Gresdal@gmail.com@bb0b4684-9848-0410-a503-375951d3d4f2>
2008-04-22 01:23:41 +0000

    Fixed indentation.

--- trunk/Java/jME/myjME/utils/FIFO.java
+++ trunk/Java/jME/myjME/utils/FIFO.java
@@ -6,42 +6,42 @@ package myjME.utils;
 * @author Anders Fongen
 */
 public class FIFO/*<E>*/ {
-    private int front, back, capacity;
-    private Condition dataElements, freeSpace;
-    private Object[] storage;
+	private int front, back, capacity;
+	private Condition dataElements, freeSpace;
+	private Object[] storage;
 	
-    public FIFO(int cap) {
+	public FIFO(int cap) {
 		storage = new Object[cap];
 		capacity = cap;
 		dataElements = new Condition(0);
 		freeSpace = new Condition(cap);
 		front = 0; back = 0;
-    }
- 
-    private synchronized <E> void in(Object obj) {
-       // Put data into ring buffer
-       storage[back] = obj;
-       back = (back+1) % capacity;
-    }
- 
-    private synchronized <E> Object out() {
-       // Get data from ring buffer
-       Object obj;
-       obj = storage[front];
-       front = (front+1) % capacity;
-       return obj;
-    }
- 
-    public <E> void put(Object obj) {
-       freeSpace.condWait();
-       in(obj);
-       dataElements.condNotify();
-    }
- 
-    public <E> Object get() {
-       dataElements.condWait();
-       Object obj = out();
-       freeSpace.condNotify();
-       return obj;
-    }
- } 
+	}
+
+	private synchronized <E> void in(Object obj) {
+		// Put data into ring buffer
+		storage[back] = obj;
+		back = (back+1) % capacity;
+	}
+
+	private synchronized <E> Object out() {
+		// Get data from ring buffer
+		Object obj;
+		obj = storage[front];
+		front = (front+1) % capacity;
+		return obj;
+	}
+
+	public <E> void put(Object obj) {
+		freeSpace.condWait();
+		in(obj);
+		dataElements.condNotify();
+	}
+
+	public <E> Object get() {
+		dataElements.condWait();
+		Object obj = out();
+		freeSpace.condNotify();
+		return obj;
+	}
+} 
