f298ca39edff29b321a6b76878d1a03648f5fa67
Robert.Gresdal@gmail.com <Robert.Gresdal@gmail.com@bb0b4684-9848-0410-a503-375951d3d4f2>
2008-04-22 01:17:45 +0000

    Added a thread safe FIFO utility class.

---
+++ trunk/Java/jME/myjME/utils/Condition.java
@@ -0,0 +1,16 @@
+package myjME.utils;
+
+public class Condition {
+	   private int fulfilled;
+	   public Condition(int initial) { fulfilled = initial; }
+	   public Condition() { this(0); }
+	   public synchronized void condNotify() {
+	      fulfilled++; notify(); 
+	   }
+	   public synchronized void condWait() {
+	      while (fulfilled==0)
+	         try { wait(); }
+	         catch (InterruptedException e) {}
+	      fulfilled--;
+	   }
+	} 
---
+++ trunk/Java/jME/myjME/utils/FIFO.java
@@ -0,0 +1,47 @@
+package myjME.utils;
+
+/**
+* Keeps a ring array of objects with thread-safe put and get methods.
+* 
+* @author Anders Fongen
+*/
+public class FIFO/*<E>*/ {
+    private int front, back, capacity;
+    private Condition dataElements, freeSpace;
+    private Object[] storage;
+	
+    public FIFO(int cap) {
+		storage = new Object[cap];
+		capacity = cap;
+		dataElements = new Condition(0);
+		freeSpace = new Condition(cap);
+		front = 0; back = 0;
+    }
+ 
+    private synchronized <E> void in(Object obj) {
+       // Put data into ring buffer
+       storage[back] = obj;
+       back = (back+1) % capacity;
+    }
+ 
+    private synchronized <E> Object out() {
+       // Get data from ring buffer
+       Object obj;
+       obj = storage[front];
+       front = (front+1) % capacity;
+       return obj;
+    }
+ 
+    public <E> void put(Object obj) {
+       freeSpace.condWait();
+       in(obj);
+       dataElements.condNotify();
+    }
+ 
+    public <E> Object get() {
+       dataElements.condWait();
+       Object obj = out();
+       freeSpace.condNotify();
+       return obj;
+    }
+ } 
