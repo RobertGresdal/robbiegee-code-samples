39b8b87d3a19bfca3e5d344fe4dc94e68f977e57
robert.gresdal <Robert.Gresdal@gmail.com>
2008-09-17 16:48:25 +0000

    Generates schemas, although Zend complains it can't find the models. I'm not sure why that happens because we set up autoloading of models with Doctrine.

--- PHP/zend_fw_test/trunk/data/db/schema/generate-db.php
+++ PHP/zend_fw_test/trunk/data/db/schema/generate-db.php
@@ -9,12 +9,13 @@ set_include_path(dirname(__FILE__) . '/../../../library' . PATH_SEPARATOR . get_
 require_once 'Zend/Loader.php';
 Zend_Loader::registerAutoload();
 
-include_once dirname(__FILE__) . '/../application/bootstrap.php';
+include_once dirname(__FILE__) . '/../../../application/bootstrap.php';
 
 
 echo 'Generating models from schemas'.PHP_EOL;
 try {
-    Doctrine::generateModelsFromYaml('user.yml', APPLICATION_PATH . '/models');
+    Doctrine::generateModelsFromYaml('user.yml', APPLICATION_PATH.'/models');
+    Doctrine::createTablesFromArray(array('User'));
 } catch( Exception $e) {
     echo 'AN ERROR HAS OCCURED:'.PHP_EOL;
     echo $e->getMessage().PHP_EOL;
