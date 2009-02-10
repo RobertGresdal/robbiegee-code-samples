2873aa6a4df8f009136dee50d89867cb90d9d329
Robert.Gresdal <Robert.Gresdal@bb0b4684-9848-0410-a503-375951d3d4f2>
2009-02-10 22:39:17 +0000

    Cleaned up some code, indentations, moved a blacklist item wrongly placed in the if-google-then-block and renamed the google blocking boolean variable.

--- trunk/JavaScript/userjs/adblock/adblock.js
+++ trunk/JavaScript/userjs/adblock/adblock.js
@@ -1,75 +1,74 @@
-// Settings:
-window.jsblock = {}; // do not touch
-//jsblock.verbosity = 3; // reporting in error window, set to 0 for keeping it quiet
-jsblock.v = {};
-jsblock.v.blocked = false; // report blocked srcipts found
-jsblock.v.suspects = true; // report suspected scripts found
-jsblock.v.externals = true; // report external scripts found
-jsblock.trackGoogle = true; // google is usually nice. their tracker does not track between sites and their scripts are very fast.
+// Settings
+window.jsblock = {
+	'v' : {
+		'blocked':false, // report blocked srcipts found
+		'suspects':true, // report suspected scripts found
+		'externals':true, // report external scripts found
+		'blockGoogle':true // google is usually nice. their tracker does not track between sites and their scripts are very fast.
+	}
+}; 
+// Blacklist:
 jsblock.list = [
-    'a.tribalfusion.com/j.ad',
+	'a.tribalfusion.com/j.ad',
 	'ad.doubleclick.net/adj',
 	'ad.no.doubleclick.net/adj',
 	'adserver.adtech.de',
-    'adsremote.scripps.com',
+	'adsremote.scripps.com',
 	'annonser.dagbladet.no',
 	'basefarm.net/RealMedia/ads',
 	'advertising.com/wrapper/aceUAC.js',
-    'core.insightexpressai.com/adServer/',
-    'edge.quantserve.com/quant.js', // did some cookie stuff. dunno what it is but it's probably a tracker
-    'hstno.tradedoubler.com',
+	'core.insightexpressai.com/adServer/',
+	'edge.quantserve.com/quant.js', // did some cookie stuff. dunno what it is but it's probably a tracker
+	'googleads.g.doubleclick.net/pagead/test_domain.js',
+	'hstno.tradedoubler.com',
 	'js.adsonar.com',
-    'o.aolcdn.com/ads/adsWrapper.js',
-    'oascentral.dilbert.com/RealMedia/ads',
-    'st.sageanalyst.net',
+	'o.aolcdn.com/ads/adsWrapper.js',
+	'oascentral.dilbert.com/RealMedia/ads',
+	'st.sageanalyst.net',
 	'track.adform.net'
 ];
-
 
 //
 // Do not change below here unless you know what you are doing
 //
 jsblock.exrx = new RegExp('^http:\/\/([a-zA-Z0-9\u0080-\u00ff-.]*)?'+location.hostname); 
-if(jsblock.trackGoogle){
-    //jsblock.list.push('ssl.google-analytics.com');
-    jsblock.list.push('google-analytics.com');
-    jsblock.list.push('googlesyndication.com/pagead/show_ads.js');
-    jsblock.list.push('googleads.g.doubleclick.net/pagead/test_domain.js');
+if(jsblock.blockGoogle){
+	jsblock.list.push('google-analytics.com');
+	jsblock.list.push('googlesyndication.com/pagead/show_ads.js');
 }
 /**
 * reverse index of. does str.indexOf(array items)
 * @return matched array index
 */
 Array.prototype.rindexOf = function(str){
-    for(var i=0,end=this.length;i<end;i++){
-        if(str.indexOf(this[i]) > -1)return i;
-    }
-    return -1;
+	for(var i=0,end=this.length;i<end;i++){
+		if(str.indexOf(this[i]) > -1)return i;
+	}
+	return -1;
 }
-
-
+// listen for script loading
 window.opera.addEventListener(
-    'BeforeExternalScript',
-    function (e) {
-        var src = e.element.getAttribute('src');
-        var isInternal = src.match(window.jsblock.exrx);
-        // is the script an external script?
-        if(src.match(/^http:\/\//)  && !isInternal){
-            // Does it belong to any of the items in the block list?
-            if(jsblock.list.rindexOf(src)>-1){
-                if(jsblock.v.blocked)
-                    opera.postError('ADBLOCK script from '+location.hostname+' loads '+src);
-                e.preventDefault();
-            } else if(jsblock.v.suspects) {
-                if(src.match('ad') || src.match('analys') || src.match('track')) {
-                    opera.postError('ADBLOCK, Suspect (ad|analys|track): '+location.hostname+' loads '+src);
-                } 
-            } else if(jsblock.v.external) {
-                if(src.match(/^http:\/\//) && !isInternal) {
-                    opera.postError('ADBLOCK, Suspect (external script): '+location.hostname+' loads '+src);
-                }
-            }
-        }
-    },
-    false
+	'BeforeExternalScript',
+	function (e) {
+		var src = e.element.getAttribute('src');
+		var isInternal = src.match(window.jsblock.exrx);
+		// is the script an external script?
+		if(src.match(/^http:\/\//)  && !isInternal){
+			// Does it belong to any of the items in the block list?
+			if(jsblock.list.rindexOf(src)>-1){
+				if(jsblock.v.blocked)
+					opera.postError('ADBLOCK script from '+location.hostname+' loads '+src);
+				e.preventDefault();
+			} else if(jsblock.v.suspects) {
+				if(src.match('ad') || src.match('analys') || src.match('track')) {
+					opera.postError('ADBLOCK, Suspect (ad|analys|track): '+location.hostname+' loads '+src);
+				} 
+			} else if(jsblock.v.external) {
+				if(src.match(/^http:\/\//) && !isInternal) {
+					opera.postError('ADBLOCK, Suspect (external script): '+location.hostname+' loads '+src);
+				}
+			}
+		}
+	},
+	false
 );
